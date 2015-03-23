dd3b030168fbf36309933c407d6b36caf5558e94
Simon Legner <Simon.Legner@gmail.com>
2015-03-23 10:02:00 +0100

    i3pystatus: conform with PIP-8

--- .bin/i3pystatus_.py
+++ .bin/i3pystatus_.py
@@ -3,15 +3,18 @@ import i3pystatus
 
 status = i3pystatus.Status(standalone=True)
 
-status.register("clock",
+status.register(
+    "clock",
     format="%Y-%m-%d %H:%M:%S",)
 
-status.register("weather",
+status.register(
+    "weather",
     location_code="AUXX0010",
     interval=600,
     format="INN {current_temp}",)
 
-status.register("battery",
+status.register(
+    "battery",
     format="B {status}{percentage:.0f}%",
     full_color="#ffffff",
     not_present_text="",
@@ -21,40 +24,49 @@ status.register("battery",
         "FULL": "",
     },)
 
-status.register("network_traffic",
+status.register(
+    "network_traffic",
     format="{interface} ↗{bytes_sent}kB/s ↘{bytes_recv}kB/s",
     hide_down=True,
     interface="wlan0",)
 
-status.register("network_traffic",
+status.register(
+    "network_traffic",
     format="{interface} ↗{bytes_sent}kB/s ↘{bytes_recv}kB/s",
     hide_down=True,
     interface="eth0",)
 
-status.register("network_traffic",
+status.register(
+    "network_traffic",
     format="{interface} ↗{bytes_sent}kB/s ↘{bytes_recv}kB/s",
     hide_down=True,
     interface="usb0",)
 
-status.register("load",
+status.register(
+    "load",
     format="L {avg1} {avg5}",)
 
-status.register("temp",
+status.register(
+    "temp",
     format="{temp:.0f}°C",)
 
-status.register("cpu_usage",
+status.register(
+    "cpu_usage",
     format="CPU {usage}%",)
 
-status.register("mem",
+status.register(
+    "mem",
     divisor=1024**3,
     color="#ffffff",
     format="MEM {used_mem}/{total_mem}GB",)
 
-status.register("disk",
+status.register(
+    "disk",
     path="/home",
     format="/home ◕ {avail}G",)
 
-status.register("disk",
+status.register(
+    "disk",
     path="/",
     format="/ ◕ {avail}G",)
 
