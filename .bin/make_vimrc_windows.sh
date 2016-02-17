9d88396a3a3c012ce691b47a0296fd58f8de3ba1
Simon Legner <Simon.Legner@gmail.com>
2016-02-17 10:39:04 +0100

    make_vimrc_windows: display output files

--- .bin/make_vimrc_windows.sh
+++ .bin/make_vimrc_windows.sh
@@ -2,3 +2,4 @@
 
 cat $HOME/.vim/config/*.vim $HOME/.vimrc | grep --invert-match --ignore-case --extended-regexp '[bv]undle' > /tmp/_vimrc
 cat $HOME/.gvimrc | grep --invert-match 'vimrc$' > /tmp/_gvimrc
+ls -l /tmp/_vimrc /tmp/_gvimrc
