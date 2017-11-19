c1c464a813568b58a3953e06ac9d83f72076f7ed
Simon Legner <Simon.Legner@gmail.com>
2017-11-19 11:20:49 +0100

    make_vimrc_windows: update for vim-plug

--- .bin/make_vimrc_windows.sh
+++ .bin/make_vimrc_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat $HOME/.vim/config/*.vim $HOME/.vimrc | grep --invert-match --ignore-case --extended-regexp '[bv]undle' > /tmp/_vimrc
+cat $HOME/.vim/config/*.vim $HOME/.vimrc | grep --invert-match --ignore-case --extended-regexp 'plug[ #]' > /tmp/_vimrc
 cat $HOME/.gvimrc | grep --invert-match 'vimrc$' > /tmp/_gvimrc
 ls -l /tmp/_vimrc /tmp/_gvimrc
