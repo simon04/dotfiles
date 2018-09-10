4bebbfd375f5e97db509d25d9d3e0498fdbc958f
Simon Legner <Simon.Legner@gmail.com>
2018-09-10 23:10:33 +0200

    config.fish: set environment variables for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,8 @@ if test -x /usr/bin/nvim
 else
   set --export EDITOR vim
 end
+
+set --export FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(code {})+abort'"
+set --export FZF_CTRL_T_OPTS "--preview 'bat {}'"
+set --export FZF_DEFAULT_COMMAND "fd --max-depth 3"
+set --export FZF_ALT_C_COMMAND "fd --max-depth 3"
