274dfb38def6f4e29907b0dc8237467e1805cd5a
Simon Legner <Simon.Legner@gmail.com>
2019-02-03 09:20:13 +0100

    xinitrc/clipboard: fix pidof

--- .xinitrc.d/clipboard.sh
+++ .xinitrc.d/clipboard.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [[ -x /usr/bin/parcellite ]]; then
-  pid parcellite || exec parcellite &
+  pidof parcellite || exec parcellite &
 elif [[ -x /usr/bin/copyq ]]; then
-  pid copyq || exec copyq &
+  pidof copyq || exec copyq &
 elif [[ -x /usr/bin/clipit ]]; then
   pidof clipit || exec clipit &
 fi
