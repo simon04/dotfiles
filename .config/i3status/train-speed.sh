748c15fc446109006834414e2bc950c9325f8160
Simon Legner <Simon.Legner@gmail.com>
2019-08-12 09:33:38 +0200

    i3status: support ICE train-speed

--- .config/i3status/train-speed.sh
+++ .config/i3status/train-speed.sh
@@ -3,4 +3,6 @@ if [[ $(netctl is-active wlp58s0-OEBB) = active ]]; then
   echo -n "railjet: "
   curl --silent http://railnet.oebb.at/api/speed
   echo " km/h"
+elif [[ $(netctl is-active wlp58s0-WIFIonICE) = active ]]; then
+  echo "ice: $(curl --silent https://iceportal.de/api1/rs/status | jq .speed) km/h"
 fi
