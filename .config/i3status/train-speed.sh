0ab14d7803392b7c672afb5c96d4c8f98b068904
Simon Legner <Simon.Legner@gmail.com>
2019-09-19 17:14:32 +0200

    i3status: railnet uses HTTPS (train-speed)

--- .config/i3status/train-speed.sh
+++ .config/i3status/train-speed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [[ $(netctl is-active wlp58s0-OEBB) = active ]]; then
-  echo "railjet: $(curl --silent http://railnet.oebb.at/api/speed) km/h"
+  echo "railjet: $(curl --silent https://railnet.oebb.at/api/speed) km/h"
 elif [[ $(netctl is-active wlp58s0-WIFIonICE) = active ]]; then
   echo "ice: $(curl --silent https://iceportal.de/api1/rs/status | jq .speed) km/h"
 fi
