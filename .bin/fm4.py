20820317ea212c4876e7f6f5158d92f9e8272aca
Simon Legner <Simon.Legner@gmail.com>
2019-12-06 21:36:45 +0100

    fm4: robustness

--- .bin/fm4.py
+++ .bin/fm4.py
@@ -4,9 +4,12 @@ import requests
 url = 'https://audioapi.orf.at/fm4/api/json/current/live'
 live = requests.get(url).json()
 tracks = live[0]['items']
-track_now = [i for i in tracks if i['state'] == 'P'][0]
-
-if 'interpreter' in track_now:
-    print('FM4:', track_now['interpreter'], '–', track_now['title'])
+tracks_now = [i for i in tracks if i['state'] == 'P']
+if not tracks_now:
+    print('FM4: –')
+elif 'title' in tracks_now[0] and 'interpreter' in tracks_now[0]:
+    print('FM4:', tracks_now[0]['interpreter'], '–', tracks_now[0]['title'])
+elif 'title' in tracks_now[0]:
+    print('FM4:', tracks_now[0]['title'])
 else:
-    print('FM4:', track_now['title'])
+    print('FM4: –')
