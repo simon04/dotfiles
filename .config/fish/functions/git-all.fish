657a4cb8b2309ed718e2ad45b577925f64c668fa
Simon Legner <Simon.Legner@gmail.com>
2020-01-30 12:39:28 +0100

    git-all: operate recursively

--- .config/fish/functions/git-all.fish
+++ .config/fish/functions/git-all.fish
@@ -1,6 +1,6 @@
 function git-all
     echo "Running 'git $argv' on all subdirectories..."
-    ls --directory */.git | \
+    find -name .git | \
         xargs dirname | \
         xargs --replace sh -c "echo; echo == {} ==; git -C {} $argv"
 end
