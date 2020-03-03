bf3721765ba7990b818342fa130d615b3aee35e9
Simon Legner <Simon.Legner@gmail.com>
2020-03-03 23:48:00 +0100

    gh: rename to ghh

--- .config/fish/functions/ghh.fish
+++ .config/fish/functions/ghh.fish
@@ -1,4 +1,4 @@
-function gh --description 'Clones a git repository to src/' --argument repo
+function ghh --description 'Clones a git repository to src/' --argument repo
 	set project (basename $repo)
   set destination $HOME/src/$project
   if test -d $destination
