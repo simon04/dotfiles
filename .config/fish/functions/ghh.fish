1746937f4138e4f8894b4738ededbaec5adac12f
Simon Legner <Simon.Legner@gmail.com>
2021-11-18 10:11:23 +0100

    ghh: use git clone

--- .config/fish/functions/ghh.fish
+++ .config/fish/functions/ghh.fish
@@ -4,7 +4,7 @@ function ghh --description 'Clones a git repository to src/' --argument repo
   if test -d $destination
     cd $destination
   else
-    gh repo clone $repo $destination
+    git clone $repo $destination
     cd $destination
   end
 end
