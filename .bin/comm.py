de8850af247608b80144fca329354af394ce1eb9
Simon Legner <Simon.Legner@gmail.com>
2022-10-21 22:03:27 +0200

    .py: format using black
    https://github.com/psf/black

--- .bin/comm.py
+++ .bin/comm.py
@@ -1,9 +1,11 @@
-#!/bin/env python
+#!/usr/bin/env python3
 import sys
 
 
 def file_as_set(file: str) -> set[str]:
-    with open(file, ) as f:
+    with open(
+        file,
+    ) as f:
         lines = [line.strip() for line in f.readlines()]
         return set(lines)
 
@@ -22,23 +24,28 @@ def parse_op():
     if len(sys.argv) < 3:
         usage()
     op = sys.argv[1]
-    if op == 'difference' or op == '-':
+    if op == "difference" or op == "-":
         return set.difference
-    elif op == 'intersection' or op == '&':
+    elif op == "intersection" or op == "&":
         return set.intersection
-    elif op == 'symmetric_difference' or op == '^':
+    elif op == "symmetric_difference" or op == "^":
         return set.symmetric_difference
-    elif op == 'union' or op == '|':
+    elif op == "union" or op == "|":
         return set.union
     else:
         usage()
 
 
 def usage():
-    print('Usage: ', sys.argv[0], 'difference|intersection|symmetric_difference|union', 'FILES...')
-    print('Read lines from FILES and perform given set operation')
+    print(
+        "Usage: ",
+        sys.argv[0],
+        "difference|intersection|symmetric_difference|union",
+        "FILES...",
+    )
+    print("Read lines from FILES and perform given set operation")
     exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- .bin/fm4.py
+++ .bin/fm4.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python3
+
 """Fetches the currently playing FM4 track from orf.at"""
 import requests
 
-url = 'https://audioapi.orf.at/fm4/api/json/current/live'
+url = "https://audioapi.orf.at/fm4/api/json/current/live"
 live = requests.get(url).json()
-tracks = live[0]['items']
-tracks_now = [i for i in tracks if i['state'] == 'P']
+tracks = live[0]["items"]
+tracks_now = [i for i in tracks if i["state"] == "P"]
 if not tracks_now:
-    print('FM4: –')
-elif 'title' in tracks_now[0] and 'interpreter' in tracks_now[0]:
-    print('FM4:', tracks_now[0]['interpreter'], '–', tracks_now[0]['title'])
-elif 'title' in tracks_now[0]:
-    print('FM4:', tracks_now[0]['title'])
+    print("FM4: –")
+elif "title" in tracks_now[0] and "interpreter" in tracks_now[0]:
+    print("FM4:", tracks_now[0]["interpreter"], "–", tracks_now[0]["title"])
+elif "title" in tracks_now[0]:
+    print("FM4:", tracks_now[0]["title"])
 else:
-    print('FM4: –')
+    print("FM4: –")
