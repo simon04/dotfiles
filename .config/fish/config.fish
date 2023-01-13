fbdf99e80c3d81e4f3060731cf87075fcc6be6f5
Simon Legner <Simon.Legner@gmail.com>
2023-01-14 00:11:34 +0100

    env: share common environment variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
+export (envsubst < .env)
+
 command --query nvim && set --export EDITOR nvim || set --export EDITOR vim
 
 command --query firefox && set --export BROWSER firefox
 command --query pgrep && pgrep chromium >/dev/null && set --export BROWSER chromium
 command --query pgrep && pgrep microsoft-edge >/dev/null && set --export BROWSER microsoft-edge
 command --query pgrep && pgrep msedge >/dev/null && set --export BROWSER msedge
-
-set --export TIME_STYLE long-iso
 
 set --erase SSH_AGENT_PID
 set --export SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
