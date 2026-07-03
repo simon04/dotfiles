6d4e951dc501a0f0e0f44748d46644da60100bb3
Simon Legner <Simon.Legner@gmail.com>
2026-07-03 19:50:47 +0200

    git-all in Python

--- .config/fish/functions/git-all-recursive.fish
+++
@@ -1,6 +0,0 @@
-function git-all-recursive
-    echo "Running 'git $argv' recursively on all subdirectories..."
-    find -name .git | \
-        xargs dirname | \
-        xargs --replace sh -c "echo; echo == {} ==; git -C {} $argv"
-end
--- .config/fish/functions/git-all.fish
+++
@@ -1,6 +0,0 @@
-function git-all
-    echo "Running 'git $argv' on all subdirectories..."
-    /usr/bin/ls --directory */.git | \
-        /usr/bin/xargs dirname | \
-        /usr/bin/xargs --replace sh -c "echo; echo == {} ==; git --no-pager -C {} $argv"
-end
