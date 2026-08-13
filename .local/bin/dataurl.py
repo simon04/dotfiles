5d5575627bbbce077dc3a62244c259870bd27722
Simon Legner <Simon.Legner@gmail.com>
2026-08-13 15:02:13 +0200

    dataurl: use mimetypes.guess_file_type

--- .local/bin/dataurl.py
+++ .local/bin/dataurl.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 import base64
-import subprocess
+import mimetypes
 import sys
 
 for filename in sys.argv[1:]:
-    mimetype = subprocess.run(
-        ["file", "-b", "--mime-type", filename],
-        capture_output=True,
-        check=True,
-        text=True,
-    ).stdout.strip()
+    mimetype = mimetypes.guess_file_type(filename)[0] or "application/octet-stream"
     with open(filename, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
     print(f"data:{mimetype};base64,{encoded}")
