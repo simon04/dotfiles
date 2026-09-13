8f27f656900a04d6c68e8df88b706fe0dbad70b5
Simon Legner <Simon.Legner@gmail.com>
2026-09-13 09:50:20 +0200

    adb-pull: --local-path

--- .local/bin/adb-pull.py
+++ .local/bin/adb-pull.py
@@ -3,13 +3,15 @@ import argparse
 import datetime
 import pathlib
 import re
+import shutil
 import subprocess
 
 
 class Arguments:
     start: datetime.date
     end: datetime.date
-    path: str
+    remote_path: str
+    local_path: pathlib.Path | None
     target: pathlib.Path
 
 
@@ -18,27 +20,47 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("start", type=datetime.date.fromisoformat)
 parser.add_argument("end", type=datetime.date.fromisoformat)
-parser.add_argument("--path", default="/sdcard/DCIM/Camera/")
+parser.add_argument("--remote-path", default="/sdcard/DCIM/Camera/")
+parser.add_argument("--local-path", type=pathlib.Path)
 parser.add_argument("--target", default="~/Fotos/", type=pathlib.Path)
 args = parser.parse_args(namespace=Arguments)
 args.target = args.target.expanduser()
 
+
+def date_in_range(name: str) -> datetime.date | None:
+    if not (m := re.search(r"\d{4}\d{2}\d{2}", name)):
+        return None
+    d = datetime.datetime.strptime(m.group(), "%Y%m%d").date()
+    return d if args.start <= d <= args.end else None
+
+
+def make_target(date: datetime.date) -> pathlib.Path:
+    target = args.target.joinpath(str(date.year)).joinpath(date.isoformat())
+    target.mkdir(parents=True, exist_ok=True)
+    return target
+
+
+if args.local_path:
+    images = [
+        (img, d)
+        for img in args.local_path.expanduser().iterdir()
+        if img.is_file()
+        if (d := date_in_range(img.name))
+    ]
+    for img, date in sorted(images):
+        shutil.copy2(img, make_target(date))
+    raise SystemExit
+
 p = subprocess.run(
-    ["adb", "shell", "ls", args.path],
+    ["adb", "shell", "ls", args.remote_path],
     capture_output=True,
     check=True,
     text=True,
 )
 
-images = [
-    (img, d)
-    for img in p.stdout.splitlines()
-    if (m := re.search(r"\d{4}\d{2}\d{2}", img))
-    if (d := datetime.datetime.strptime(m.group(), "%Y%m%d").date())
-    if args.start <= d <= args.end
-]
+images = [(img, d) for img in p.stdout.splitlines() if (d := date_in_range(img))]
 
 for img, date in sorted(images):
-    target = args.target.joinpath(str(date.year)).joinpath(date.isoformat())
-    target.mkdir(parents=True, exist_ok=True)
-    subprocess.run(["adb", "pull", args.path + img, str(target)], check=True)
+    subprocess.run(
+        ["adb", "pull", args.remote_path + img, str(make_target(date))], check=True
+    )
